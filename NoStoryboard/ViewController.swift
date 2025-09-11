a5c0e86f975d4444d9c9e893039294f2226b1743
minhl <minhl@scb.com.vn>
2025-09-11 08:14:18 +0700

    Initial Commit

---
+++ NoStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoStoryboard
+//
+//  Created by Minh on 11/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
