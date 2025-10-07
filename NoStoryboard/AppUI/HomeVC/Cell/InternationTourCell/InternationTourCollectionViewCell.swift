b7e25d109084fc6bb823d1d0762e3c4424ff543f
minhl <minhl@scb.com.vn>
2025-10-07 11:18:54 +0700

    adjust property in UI and rename  tp ElementDetailItemTourCollectionViewCell

---
+++ NoStoryboard/AppUI/HomeVC/Cell/InternationTourCell/InternationTourCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  InternationTourCollectionViewCell.swift
+//  NoStoryboard
+//
+//  Created by Minh on 6/10/25.
+//
+
+import UIKit
+
+class InternationTourCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ NoStoryboard/AppUI/HomeVC/Cell/SubCell/ElementDetailItemTourCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  OutstandingItemCollectionViewCell.swift
+//  NoStoryboard
+//
+//  Created by Minh on 25/9/25.
+//
+
+import UIKit
+protocol OutstandingItemCollectionViewCellDelegate {
+    func didSelectItem(at indexPath: IndexPath)
+}
+
+class ElementDetailItemTourCollectionViewCell: UICollectionViewCell {
+    static let identifier = "OutstandingItemCollectionViewCell"
+    var delegate: OutstandingItemCollectionViewCellDelegate?
+    @IBOutlet weak var imgViewItem: UIImageView!
+    
+    @IBOutlet weak var titleNameItem: UILabel!
+    
+    @IBOutlet weak var Subtile: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        imgViewItem.layer.cornerRadius = 10
+    }
+
+    
+    override func prepareForReuse() {
+        imgViewItem.image = nil
+        titleNameItem.text = nil
+        Subtile.text = nil
+        
+    }
+}
